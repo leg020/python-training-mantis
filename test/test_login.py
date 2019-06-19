8a09ff3920ecf3ddf064f5ab8d34ba2293208343
Александр <leg020@inbox.ru>
2019-06-18 20:27:45 -0700

    Реализовал тест логина

--- test/test_login.py
+++ test/test_login.py
@@ -1,6 +1,5 @@
 __author__ = 'Alex'
 
 def test_login(app):
-    #app.open_home_page()
-    app.session('administrator', 'root')
+    app.session.login('administrator', 'root')
     assert app.session.is_logged_in_as('administrator')
