d78edb3bc2dfdc66a653f8a7d0416ebff349856d
Александр <leg020@inbox.ru>
2019-06-20 09:38:15 -0700

    Поправил загрузку из data

--- data/project.py
+++ data/project.py
@@ -10,8 +10,8 @@ def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [Project(name=random_string('name', 3),
-                    description=random_string('description', 3),
+testdata = [Project(name=random_string('name', 10),
+                    description=random_string('description', 10),
                     status=random.choice(status_list),
                     view_status=random.choice(view_status_list))]
 
