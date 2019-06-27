f2a5b9d77ccc8bc75c604b2eb4c844e1ff774f3d
Александр <leg020@inbox.ru>
2019-06-26 19:46:30 -0700

    Переделал проверки, выполнидл все с помощью soup

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -5,11 +5,9 @@ from data.project import testdata
 def test_add_project(app, data_project, db):
     project = data_project
     app.session.login('administrator', 'root')
-    old_project = db.get_project_list()
+    old_project = app.soap.get_project('administrator', "root")
     app.project.create_project(project)
-    new_project = db.get_project_list()
-    new_soup_project = app.soap.get_project('administrator', "root")
+    new_project = app.soap.get_project('administrator', "root")
     assert len(old_project) + 1 == len(new_project)
     old_project.append(project)
     assert sorted(old_project, key=Project.id_or_max) == sorted(new_project, key=Project.id_or_max)
-    assert sorted(old_project, key=Project.id_or_max) == sorted(new_soup_project, key=Project.id_or_max)
--- test/test_del_project.py
+++ test/test_del_project.py
@@ -3,14 +3,12 @@ import random
 
 def test_del_project(app, db):
     app.session.login('administrator', 'root')
-    if len(db.get_project_list()) == 0:
+    if len(app.soap.get_project('administrator', "root")) == 0:
         app.project.create_project(Project(name='test', description='test'))
-    old_projects = db.get_project_list()
+    old_projects = app.soap.get_project('administrator', "root")
     project = random.choice(old_projects)
     app.project.delete_project_by_id(project.id)
-    new_projects = db.get_project_list()
-    new_soup_project = app.soap.get_project('administrator', "root")
+    new_projects = app.soap.get_project('administrator', "root")
     assert len(old_projects) - 1 == len(new_projects)
     old_projects.remove(project)
     assert old_projects == new_projects
-    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_soup_project, key=Project.id_or_max)
